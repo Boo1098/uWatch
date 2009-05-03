4908062b2a6d4cf84b6cfd68cfc3531d9cd902b9
zowki <zowki@657dcf9f-b3fa-443b-a7e1-5e2251c1fc9b>
2009-05-03 12:37:20 +0000

    removed goto statement in lunar lander

--- trunk/uWatch-GamesMode.c
+++ trunk/uWatch-GamesMode.c
@@ -69,11 +69,16 @@ int lunarLander( int p )
 
     while ( height > 0 ) {
         if ( fuelRemaining > 0 ) {
-lunar:
             UpdateLCDline1( "ENT - burn fuel" );
             UpdateLCDline2( "MENU - stats" );
             KeyPress2 = GetDebouncedKey();
             if ( KeyPress2 == KeyMode ) return MODE_KEYMODE;
+            if ( ENTER(KeyPress2) ) {  //enter fuel to burn
+                UpdateLCDline1( "Burn fuel(0-30):" );
+                Xreg = burnAmount;
+                c = OneLineNumberEntry();
+                burnAmount = Xreg;
+            }
             if ( KeyPress2 == KeyMenu ) { //stats menu
                 UpdateLCDline1( "1=Time  2=Height" );
                 UpdateLCDline2( "3=Speed 4=Fuel" );
@@ -107,13 +112,6 @@ lunar:
                     KeyPress2 = GetDebouncedKey();
                     if ( KeyPress2 == KeyMode ) return MODE_KEYMODE;
                 }
-                goto lunar;
-            }
-            if ( ENTER(KeyPress2) ) {  //enter fuel to burn
-                UpdateLCDline1( "Burn fuel(0-30):" );
-                Xreg = burnAmount;
-                c = OneLineNumberEntry();
-                burnAmount = Xreg;
             }
         }
 
